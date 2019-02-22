00cd916faf84026f68efadbd3921798b0b93f587
wlf061 <wlf061@sina.com>
2019-02-22 16:37:33 +0800

    add numpy and pandas info

--- base/function1.py
+++ base/function1.py
@@ -1,1 +1,25 @@
-def
+"""
+函数嵌套
+"""
+def fun1():
+    def fun2():
+        print("function2")
+    print("fun1")
+    return fun2
+
+
+"""
+函数闭包
+"""
+def f0(x):
+    def f1(y):
+        print(x+y)
+    return f1
+
+if __name__=='__main__':
+    obj=fun1()
+    print('-----')
+    obj()
+
+    obj = f0(10)
+    obj(20)
